0a981c88bb930126792956395fe957dfd1867252
Ponomar <artemponomar98@gmail.com>
2019-05-06 14:56:40 +0300

    initial

---
+++ src/main/java/com/pedigree/PedigreeApplication.java
@@ -0,0 +1,13 @@
+package com.pedigree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedigreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedigreeApplication.class, args);
+	}
+
+}
